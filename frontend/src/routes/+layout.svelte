2158ed18e08c4c1592a7879b14af04a91a498816
Denys Kaminskyi <dionisiy.kdo@gmail.com>
2025-08-23 10:10:37 +0300

    frontend: setup initial page

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,12 +1,27 @@
 <script lang="ts">
-	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import Header from '$lib/Header.svelte';
+	import Footer from '$lib/Footer.svelte';
+	import '../app.css';
 
+	import { initializeToken } from '$lib/auth';
+	
 	let { children } = $props();
+
+	initializeToken();
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-{@render children?.()}
+
+<div class="flex flex-col min-h-screen">
+	<Header />
+
+	<main class="flex-1 flex mx-auto w-full flex-col p-4 max-w-7xl">
+		{@render children?.()}
+	</main>
+
+	<Footer />
+</div>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,9 +1,26 @@
 <script>
   let message = "";
-  fetch("http://127.0.0.1:8000/api/hello/")
-    .then(r => r.json())
-    .then(data => message = data.message);
+  
+  // check dashboard api based on role
+  async function fetchDashboard() {
+    try {
+      const role = localStorage.getItem("role");
+      const response = await fetch(`http://127.0.0.1:8000/api/${role}/dashboard/`, {
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${localStorage.getItem("access_token")}`
+        }
+      });
+      const data = await response.json();
+      message = data.message;
+      console.log(data);
+      
+    } catch (error) {
+      message = "Error fetching dashboard data.";
+    }
+  }
+
+  fetchDashboard();
 </script>
 
 <h1>{message}</h1>
-<h1>Abobus maximus</h1>
