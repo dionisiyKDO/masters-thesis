4745a2b74dc137b3d71fd00c661284af3ce35ce9
Denys Kaminskyi <dionisiy.kdo@gmail.com>
2025-08-23 10:09:10 +0300

    frontend: use old footer header components

---
+++ frontend/src/lib/Footer.svelte
@@ -0,0 +1,11 @@
+<script>
+</script>
+
+
+<footer class="bg-(--surface) border-t border-[--border] flex flex-col items-center justify-center p-4">
+    <p>Kaminskyi Denys <a class="font-bold" href="https://github.com/dionisiyKDO">Git</a> </p>
+</footer>
+
+
+<style>
+</style>
---
+++ frontend/src/lib/Header.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+    import { user, logout } from '$lib/auth';
+
+    const links = [
+        { name: 'Home', href: '/' },
+    ];
+</script>
+
+<header class="p-6 pb-4 mb-6 bg-(--surface) border-b border-[--border]">
+    <nav class="flex items-center justify-between">
+        {#if $user}
+            <a href="/" class="text-xl font-bold">{$user?.username}'s Blog</a>
+        {:else}
+            <a href="/" class="text-xl font-bold">Blog</a>
+        {/if}
+        
+        <div class="flex items-center gap-4">
+            
+            {#each links as link}
+                <a href={link.href}>{link.name}</a>
+            {/each}
+
+            <div class="border-l border-[--border]">
+                {#if $user}
+                    <button onclick={logout} class="min-w-16 ml-4">Logout</button>
+                {:else}
+                    <a href="/auth" class="min-w-16 inline-block ml-4">Login</a>
+                {/if}
+            </div>
+        </div>
+    </nav>
+</header>
