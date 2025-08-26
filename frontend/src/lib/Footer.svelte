04015784a4aa2cf3d488d9d5937c17212c434f53
Denys Kaminskyi <dionisiy.kdo@gmail.com>
2025-08-26 11:33:17 +0300

    frontend: fixed for styles and text

--- frontend/src/lib/Footer.svelte
+++ frontend/src/lib/Footer.svelte
@@ -2,7 +2,7 @@
 </script>
 
 
-<footer class="bg-(--surface) border-t border-[--border] flex flex-col items-center justify-center p-4">
+<footer class="bg-background border-t border-border flex flex-col items-center justify-center py-2 selection:bg-primary selection:text-primary-foreground">
     <p>Kaminskyi Denys <a class="font-bold" href="https://github.com/dionisiyKDO">Git</a> </p>
 </footer>
 
