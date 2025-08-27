22e4b30216a100c7aa4208037ac1e941fd5a83a9
Denys Kaminskyi <dionisiy.kdo@gmail.com>
2025-08-27 05:36:37 +0300

    frontend: update styles

--- frontend/src/routes/auth/+page.svelte
+++ frontend/src/routes/auth/+page.svelte
@@ -7,11 +7,11 @@
 
 	<div class="max-w-md min-w-96">
 		<form onsubmit={handleSubmit} class="flex flex-col gap-2">
-			<input class="form-input" name="username" type="text" placeholder="username" required />
+			<input class="input" name="username" type="text" placeholder="username" required />
 			<br />
-			<input class="form-input" name="password" type="password" placeholder="password" required />
+			<input class="input" name="password" type="password" placeholder="password" required />
 			<br />
-			<button class="form-button" type="submit">Login</button>
+			<button class="button" type="submit">Login</button>
 		</form>
 	</div>
 </div>
