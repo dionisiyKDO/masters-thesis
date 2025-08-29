f302a9349161b86a9238faab716ca0eb34d2719e
Denys Kaminskyi <dionisiy.kdo@gmail.com>
2025-08-29 12:28:16 +0300

    backend: add media root and url

--- backend/pneumonia_diagnosis/settings.py
+++ backend/pneumonia_diagnosis/settings.py
@@ -156,6 +156,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, STATIC_URL),
 ]
 
+MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
