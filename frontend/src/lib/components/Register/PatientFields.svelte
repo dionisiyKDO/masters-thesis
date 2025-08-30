69323d4035eba66bf9fa71ed2ed231866c347cde
Denys Kaminskyi <dionisiy.kdo@gmail.com>
2025-08-30 03:25:51 +0300

    frontend: create components for register fields

---
+++ frontend/src/lib/components/Register/PatientFields.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+    export let formData: any;
+</script>
+
+<select
+    bind:value={formData.sex}
+    name="sex"
+    class="input"
+    required
+>
+    <option value="" disabled>Select Gender</option>
+    <option value="male">Male</option>
+    <option value="female">Female</option>
+    <option value="other">Other</option>
+    <option value="prefer_not_to_say">Prefer not to say</option>
+</select>
+
+<input
+    type="date"
+    bind:value={formData.dob}
+    name="date_of_birth"
+    class="input"
+    required
+/>
+
+<input
+    bind:value={formData.contact_info}
+    placeholder="Phone Number"
+    name="phone_number"
+    type="tel"
+    class="input"
+    required
+/>
+
+<input
+    type="date"
+    bind:value={formData.medical_record_number}
+    name="date_of_birth"
+    class="input"
+    required
+/>
---
+++ frontend/src/lib/components/Register/SharedFields.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    export let formData: any;
+</script>
+
+<div class="flex gap-2">
+    <input
+        bind:value={formData.first_name}
+        placeholder="First Name"
+        name="first_name"
+        class="input"
+        required
+    />
+    <input
+        bind:value={formData.last_name}
+        placeholder="Last Name"
+        name="last_name"
+        class="input"
+        required
+    />
+</div>
+
+<input
+    bind:value={formData.email}
+    type="email"
+    placeholder="example@gmail.com"
+    name="email"
+    class="input"
+    required
+/>
