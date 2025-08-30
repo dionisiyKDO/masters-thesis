33821777d404a23a756b5f131711d5f68f97e5c8
Denys Kaminskyi <dionisiy.kdo@gmail.com>
2025-08-30 03:25:51 +0300

    frontend: create components for register fields

---
+++ frontend/src/lib/components/Register/DoctorFields.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    export let formData: any;
+</script>
+
+
+<input
+    bind:value={formData.license_number}
+    placeholder="Medical License Number"
+    name="license_number"
+    class="input"
+    required
+/>
+
+<input
+    bind:value={formData.specialization}
+    placeholder="Specialization (e.g., Cardiology, Neurology)"
+    name="specialization"
+    class="input"
+    required
+/>
