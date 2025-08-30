f2e835f84974786674a716ec3724a89321ced3dd
Denys Kaminskyi <dionisiy.kdo@gmail.com>
2025-08-30 03:25:24 +0300

    frontend: create register page

---
+++ frontend/src/routes/register/load.ts
@@ -0,0 +1,17 @@
+import { goto } from '$app/navigation';
+import { user, login } from '$lib/auth';
+
+export async function handleSubmit(event: Event) {
+	event.preventDefault();
+
+	const form = event.target as HTMLFormElement;
+	
+	// const username = form.username.value;
+	// const password = form.password.value;
+
+	// login(username, password).then((success) => {
+	// 	if (success) {
+	// 		goto('/');
+	// 	}
+	// });
+}
