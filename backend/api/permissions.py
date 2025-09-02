1bdc435eee1fe31c9d4a92bc1f96202ea0a4a85a
Denys Kaminskyi <dionisiy.kdo@gmail.com>
2025-09-02 10:11:40 +0300

    backend: permissions managing

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -1,5 +1,12 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 
+class IsAdmin(BasePermission):
+    """
+    Allows access only to users with the 'admin' role.
+    """
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.role == 'admin'
+
 class IsAdminOrReadOnly(BasePermission):
     """
     Allows full access to admin users, but read-only access to others.
